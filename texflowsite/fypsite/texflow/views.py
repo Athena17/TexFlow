e55057f4e5fb135ebb0a763f50624d400da1608f
Nourhane <49608714+Athena17@users.noreply.github.com>
2021-04-01 02:02:52 +0300

    fixed minor issue

--- texflowsite/fypsite/texflow/views.py
+++ texflowsite/fypsite/texflow/views.py
@@ -51,24 +51,26 @@ def index(request):
 
             inputText = request.POST['hidden-input']
             nlist.append(nlist[len(nlist)-1])
-            graph = run_example(inputText, nlist)
             
-            print(graph)
+            graph = run_example(inputText, nlist)
+
             nodes = {}
             edges = {}
             for node in graph['nodes']:
                 nodes[node.strip()] = 1
 
             nodes["0"] = inputText
-            
-            for edge in graph['edges']:
-                edges[edge[0].strip() + "|" + edge[1].strip() + "|" + str(edge[2]).strip() + "|"] = graph['edges'][edge].strip()
-            # dump data 
+    
             general = {'time' : 2}
+            
+            # dump data 
             gen = dumps(general)
             data1JSON = dumps(nodes) 
-            data2JSON = dumps(edges) 
-            return render(request, 'index.html', {'data1': data1JSON, 'data2': data2JSON, 'general' : gen}) 
+            data2JSON = dumps(graph['edges']) 
+            data3JSON = dumps(graph['parentPath'])
+
+            return render(request, 'index.html', {'data1': data1JSON, 'data2': data2JSON, 'data3': data3JSON,'general' : gen}) 
+
         else:
             return render(request, 'index.html',{})
     else:
